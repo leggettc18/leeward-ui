92622cb8152c396fbc46cb56499d6ae0b6a822b4
Christopher Leggett <chris@leggett.dev>
2021-03-20 13:50:36 -0400

    Removes some unneeded imports.

--- dev/serve.vue
+++ dev/serve.vue
@@ -1,12 +1,9 @@
 <script lang="ts">
-import LeButton from "@/lib-components/le-button.vue";
-import LeApp from "@/lib-components/le-app.vue";
 import { defineComponent } from "vue";
 // Uncomment import and local "components" registration if library is not registered globally.
 // import { LeewardUiSample } from '@/entry.esm';
 
 export default defineComponent({
-  components: { LeButton, LeApp },
   name: "ServeDev",
   // components: {
   //  LeewardUiSample,
